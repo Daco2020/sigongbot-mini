2ee6eb5014f1e7615460066b06f4d32cf0168968
Daco2020 <dacokim32@gmail.com>
2025-09-07 01:04:10 +0900

    chore: 세션 표기 수정

--- constants.py
+++ constants.py
@@ -25,25 +25,26 @@ DUE_DATES = [
     datetime.datetime(2025, 8, 19, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 추가2회차
     datetime.datetime(2025, 8, 26, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 추가3회차
     datetime.datetime(2025, 9, 2, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 추가4회차
-    datetime.datetime(2025, 9, 9, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 1회차
-    datetime.datetime(2025, 9, 16, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 2회차
-    datetime.datetime(2025, 9, 23, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3회차
-    datetime.datetime(2025, 9, 30, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 4회차
-    datetime.datetime(2025, 10, 7, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 5회차
-    datetime.datetime(2025, 10, 14, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 6회차
-    datetime.datetime(2025, 10, 21, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 7회차
-    datetime.datetime(2025, 10, 28, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 8회차
-    datetime.datetime(2025, 11, 4, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 9회차
-    datetime.datetime(2025, 11, 11, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 10회차
-    datetime.datetime(2025, 11, 18, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 11회차
-    datetime.datetime(2025, 11, 25, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 12회차
-    datetime.datetime(2025, 12, 2, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 추가1회차
-    datetime.datetime(2025, 12, 9, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 추가2회차
-    datetime.datetime(2025, 12, 16, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 추가3회차
-    datetime.datetime(2025, 12, 23, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 추가4회차
+    datetime.datetime(2025, 9, 9, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 1회차
+    datetime.datetime(2025, 9, 16, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 2회차
+    datetime.datetime(2025, 9, 23, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 3회차
+    datetime.datetime(2025, 9, 30, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 4회차
+    datetime.datetime(2025, 10, 7, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 5회차
+    datetime.datetime(2025, 10, 14, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 6회차
+    datetime.datetime(2025, 10, 21, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 7회차
+    datetime.datetime(2025, 10, 28, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 8회차
+    datetime.datetime(2025, 11, 4, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 9회차
+    datetime.datetime(2025, 11, 11, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 10회차
+    datetime.datetime(2025, 11, 18, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 11회차
+    datetime.datetime(2025, 11, 25, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 12회차
+    datetime.datetime(2025, 12, 2, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 추가1회차
+    datetime.datetime(2025, 12, 9, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 추가2회차
+    datetime.datetime(2025, 12, 16, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 추가3회차
+    datetime.datetime(2025, 12, 23, 5, 0, 0, tzinfo=ZoneInfo("Asia/Seoul")),  # 3기 추가4회차
 ]
 
 # 각 마감일의 설명
+# 세션 표기가 같으면 이미 제출한 것으로 처리하므로 겹치지 않게 주의할 것.
 SESSION_NAMES = [
     "준비회차",
     "0회차",
@@ -63,20 +64,20 @@ SESSION_NAMES = [
     "추가2회차",
     "추가3회차",
     "추가4회차",
-    "1회차",
-    "2회차",
-    "3회차",
-    "4회차",
-    "5회차",
-    "6회차",
-    "7회차",
-    "8회차",
-    "9회차",
-    "10회차",
-    "11회차",
-    "12회차",
-    "추가1회차",
-    "추가2회차",
-    "추가3회차",
-    "추가4회차",
+    "3기 1회차",
+    "3기 2회차",
+    "3기 3회차",
+    "3기 4회차",
+    "3기 5회차",
+    "3기 6회차",
+    "3기 7회차",
+    "3기 8회차",
+    "3기 9회차",
+    "3기 10회차",
+    "3기 11회차",
+    "3기 12회차",
+    "3기 추가1회차",
+    "3기 추가2회차",
+    "3기 추가3회차",
+    "3기 추가4회차",
 ]
